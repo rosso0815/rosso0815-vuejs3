cd2dea3ea0cbcf95fce3c5b45128d3919969a20d
rosso0815 <rosso0815@gmail.com>
2024-07-25 07:32:09 +0200

    add_method_foo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 </script>
+
+<script>
+export default {
+  data() {
+    return {
+      numEmployees: 10,
+    };
+  },
+  methods: {
+    addEmployees() {
+      this.numEmployees++;
+    },
+    subtractEmployees() {
+      this.numEmployees--;
+    },
+    foo(){
+      console.log("foo");
+    }
+  },
+};
+</script>
+
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -13,35 +35,14 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+
+    <div class="container-fluid">
+      <h1>BOOTSTRAP</h1>
+      <button class="btn btn-primary" @click="foo()">
+        BUTTON
+      </button>
+    </div>
+
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
